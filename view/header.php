138cc0338d4475221afa612d408bc236b8cb4801
tam24 <tam.escalante@gmail.com>
2017-11-27 09:07:34 +0100

    Change in header

--- view/header.php
+++ view/header.php
@@ -15,6 +15,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js"></script>
 
+   <!-- JQuery full version -->
     <script
       src="https://code.jquery.com/jquery-3.2.1.js"
       integrity="sha256-DZAnKJ/6XZ9si04Hgrsxu/8s717jcIzLy3oi35EouyE="
